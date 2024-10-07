b4633fbbfc12ed6379933228b0a97bcc9064fc1a
mMateusDSouza <Mateusduduarte18@gmail.com>
2024-10-07 15:07:30 -0300

    Estrutura Básica Pronta

---
+++ src/main/java/com/mycompany/login/app/LoginApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.login.app;
+
+/**
+ *
+ * @author mateus
+ */
+public class LoginApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
